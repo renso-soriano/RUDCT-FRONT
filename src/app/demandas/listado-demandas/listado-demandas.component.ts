f704e1a76f395718c8308b18f745570799e07645
amado Brito Lenovo <plutyn@hotmail.com>
2021-07-10 01:55:23 -0400

    ListoDataTable1.0

--- src/app/demandas/listado-demandas/listado-demandas.component.ts
+++ src/app/demandas/listado-demandas/listado-demandas.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
+import { DatatableData } from './data/datatables.data';
+import {
+  ColumnMode,
+  DatatableComponent,
+  SelectionType
+} from '@swimlane/ngx-datatable';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 declare var require: any;
 const data: any = require('../../shared/data/Demandas.json');
@@ -7,9 +15,199 @@ const data: any = require('../../shared/data/Demandas.json');
 @Component({
   selector: 'app-listado-demandas',
   templateUrl: './listado-demandas.component.html',
-  styleUrls: ['./listado-demandas.component.scss']
-})
+  styleUrls: ['./listado-demandas.component.scss', '../../../assets/sass/libs/datatables.scss'],
+  encapsulation: ViewEncapsulation.None})
 export class ListadoDemandasComponent implements OnInit {
+
+    loadingIndicator: boolean = true;
+    reorderable: boolean = true;
+
+  // public
+  public contentHeader: object;
+
+  // row data
+  //public rows = DatatableData;
+  public rows = data;
+
+  // column header
+  public columns = [
+    { name:'Codigo', prop: 'Codigo Demanda'},
+    { prop: 'Demanda'},
+    { prop: 'Año'},
+    { name:'Origen', prop: 'Origen Demanda'},
+    { name:'Estado', prop: 'Estado Ejecucion'}
+];
+
+  // multi Purpose datatable Row data
+  public multiPurposeRows = DatatableData;
+
+  public ColumnMode = ColumnMode;
+
+  @ViewChild(DatatableComponent) table: DatatableComponent;
+  @ViewChild('tableRowDetails') tableRowDetails: any;
+  @ViewChild('tableResponsive') tableResponsive: any;
+
+  public expanded: any = {};
+
+  public editing = {};
+
+  public chkBoxSelected = [];
+  public SelectionType = SelectionType;
+
+  // server side row data
+  public serverSideRowData;
+
+  // private
+  private tempData = [];
+  private multiPurposeTemp = [];
+
+  /**
+   * inlineEditingUpdate
+   *
+   * @param event
+   * @param cell
+   * @param rowIndex
+   */
+  inlineEditingUpdate(event, cell, rowIndex) {
+    this.editing[rowIndex + '-' + cell] = false;
+    this.rows[rowIndex][cell] = event.target.value;
+    this.rows = [...this.rows];
+  }
+
+  /**
+   * filterUpdate
+   *
+   * @param code
+   */
+  filterUpdate(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.tempData.filter(function (d) {
+      return d.Demanda.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
+
+  /**
+   * rowDetailsToggleExpand
+   *
+   * @param row
+   */
+  rowDetailsToggleExpand(row) {
+    this.tableRowDetails.rowDetail.toggleExpandRow(row);
+  }
+
+  /**
+   * toggleExpandRowResponsive
+   *
+   * @param row
+   */
+  toggleExpandRowResponsive(row) {
+    this.tableResponsive.rowDetail.toggleExpandRow(row);
+  }
+
+  /**
+   * customChkboxOnSelect
+   *
+   * @param { selected }
+   */
+  customChkboxOnSelect({ selected }) {
+    this.chkBoxSelected.splice(0, this.chkBoxSelected.length);
+    this.chkBoxSelected.push(...selected);
+  }
+
+  /**
+   * serverSideSetPage
+   *
+   * @param event
+   */
+  serverSideSetPage(event) {
+    this.http
+      .get('assets/data/datatable-data.json')
+      .pipe(map((data) => data as Array<any>))
+      .subscribe((data) => {
+        this.serverSideRowData = data;
+      });
+  }
+
+  /**
+   * MultiPurposeFilterUpdate
+   *
+   * @param event
+   */
+  MultiPurposeFilterUpdate(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.multiPurposeTemp.filter(function (d) {
+      return d.full_name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.multiPurposeRows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {HttpClient} http
+   */
+  constructor(private http: HttpClient) {
+    this.tempData = DatatableData;
+    this.multiPurposeTemp = DatatableData;
+    setTimeout(() => { this.loadingIndicator = false; }, 1500);
+  }
+
+  // Lifecycle Hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * On init
+   */
+  ngOnInit() {
+    // Initially load first page
+    this.serverSideSetPage({ offset: 0 });
+
+    // content header
+    this.contentHeader = {
+      headerTitle: 'Datatables',
+      actionButton: true,
+      breadcrumb: {
+        type: '',
+        links: [
+          {
+            name: 'Home',
+            isLink: true,
+            link: '#'
+          },
+          {
+            name: 'Forms & Tables',
+            isLink: true,
+            link: ''
+          },
+          {
+            name: 'Datatables',
+            isLink: false
+          }
+        ]
+      }
+    };
+  }
+}
+
+
+
+
+
+
+/* implements OnInit {
 
   ngOnInit(): void {
   }
@@ -19,12 +217,14 @@ export class ListadoDemandasComponent implements OnInit {
   reorderable: boolean = true;
 
   columns = [
-      { prop: 'Codigo Demanda'},
-      { prop: 'Año'},
+      { name:'Codigo', prop: 'Codigo Demanda'},
       { prop: 'Demanda'},
-      { prop: 'Origen Demanda'},
+      { prop: 'Año'},
+      { name:'Origen', prop: 'Origen Demanda'},
       { prop: 'Provincia'},
-      { prop: 'Estado Ejecucion'}
+      { name:'Estado', prop: 'Estado Ejecucion'},
+      { name: 'Acciones'}
+
 
   ];
 
@@ -35,4 +235,4 @@ export class ListadoDemandasComponent implements OnInit {
   }
 
 
-}
+} */
