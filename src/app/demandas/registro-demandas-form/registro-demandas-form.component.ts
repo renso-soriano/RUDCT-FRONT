3f5091f16b7cc9d6d5b892d30494dcb50ccbdbda
camil.doroteo <camil.doroteo@economia.gob.do>
2023-07-20 10:08:37 -0400

    Changes

--- src/app/demandas/registro-demandas-form/registro-demandas-form.component.ts
+++ src/app/demandas/registro-demandas-form/registro-demandas-form.component.ts
@@ -1060,7 +1060,7 @@ export class RegistroDemandasFormComponent implements OnInit, AfterViewInit {
     //     Nombre: item.nombrePolitica,
     //   };
     // });
-    this.listadoInstituciones = demanda.institucionesInvolucradas.map(
+    this.listadoInstituciones = demanda.institucionesInvolucradas?.map(
       (item: any) => {
         return {
           Id: item.id,
