d1eb18fd613963dcae5a13e0cade5d898e35bc88
keury.lendof <keury.lendof@economia.gob.do>
2023-08-02 14:08:40 -0400

    Instituciones involucradas en el excel

--- src/app/shared/utilidades/estados-utils.ts
+++ src/app/shared/utilidades/estados-utils.ts
@@ -6,46 +6,53 @@ import { Estados } from '../models/auth/estados.enum';
 })
 export class EstadoUtilsService {
 
-  getEstadoClass(estadoId: number): { [className: string]: boolean } {
-    return {
-        'bg-danger': estadoId === 3,
-        'bg-warning': estadoId === 2,
-        'bg-info': estadoId === 5,
-        'bg-primary': estadoId === 4,
-        'bg-secondary': estadoId === 1,
-        'bg-success': estadoId === 6,
-        'bg-dark': estadoId === 7
-      };
-  }
+  
 
-  getEstadoIdForInstitucion(institucionesInvolucradas: any[], idInstitucion: number): number | null {
-    const institucionInvolucrada = institucionesInvolucradas.find(institucion => institucion.institucionId === idInstitucion);
-    return institucionInvolucrada ? institucionInvolucrada.estadoId : null;
-  }
+    getEstadoIdForInstitucion(institucionesInvolucradas: any[], idInstitucion: number): number | null {
 
-  titleEstadoEjecucion(id:number):string{
+        const institucionInvolucrada = institucionesInvolucradas.find(institucion => institucion.institucionId === idInstitucion);
+        return institucionInvolucrada ? institucionInvolucrada.estadoId : null;
 
-    switch (id) {
-      case Estados.pendienteAsignarSectorial:
-        return 'Pendiente Asignar Sectorial';
-      case Estados.asignadoASectorial:
-        return 'Asignado A Sectorial';
-      case Estados.reasignacionSectorial:
-        return 'Reasignacion Sectorial';
-      case Estados.enProcesoDeEjecucion:
-        return 'En Proceso De Ejecucion';
-      case Estados.incluidoEnPEI:
-        return 'Incluido En PEI';
-      case Estados.programadoEnPOA:
-        return 'Programado En POA';
-      case Estados.noInciada:
-        return 'No Iniciada';
-      case Estados.ejecutado:
-        return 'Ejecutado';
-      default:
-        return '';
     }
 
-  }
+    getEstadoClass(estadoId: number): { [className: string]: boolean } {
+
+        return {
+            'bg-danger': estadoId === 3,
+            'bg-warning': estadoId === 2,
+            'bg-info': estadoId === 5,
+            'bg-primary': estadoId === 4,
+            'bg-secondary': estadoId === 1,
+            'bg-success': estadoId === 6,
+            'bg-dark': estadoId === 7,
+            'bg-light': estadoId === 24
+        };
+
+    }
+
+    titleEstadoEjecucion(id:number):string{
+
+        switch (id) {
+            case Estados.pendienteAsignarSectorial:
+                return 'Pendiente Asignar Sectorial';
+            case Estados.asignadoASectorial:
+                return 'Asignado A Sectorial';
+            case Estados.reasignacionSectorial:
+                return 'Reasignacion Sectorial';
+            case Estados.enProcesoDeEjecucion:
+                return 'En Proceso De Ejecucion';
+            case Estados.incluidoEnPEI:
+                return 'Incluido En PEI';
+            case Estados.programadoEnPOA:
+                return 'Programado En POA';
+            case Estados.noInciada:
+                return 'No Iniciada';
+            case Estados.ejecutado:
+                return 'Ejecutado';
+            default:
+                return '';
+        }
+
+    }
 
 }
